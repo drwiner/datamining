bff84237ea51d830b4ffa84271b9ed8eec51032f
drwiner <drwiner131@gmail.com>
2017-02-26 13:04:19 -0700

    clustering task

--- hierarchicalClustering.py
+++ hierarchicalClustering.py
@@ -1,5 +1,6 @@
 import numpy as np
 from itertools import product
+import math
 # import collections
 
 # NamedPoint = collections.namedtuple('NamedPoint', ['id', 'point'])
@@ -8,9 +9,18 @@ class NamedPoint:
 	def __init__(self, int_id, point):
 		self.int_id = int_id
 		self.point = point
-		self.single_link_sim = dict()
-		self.complete_link_sim = dict()
-		self.mean_link_sim = dict()
+
+class Cluster:
+	def __init__(self, points):
+		self.points = points
+		# self.single_link_sim = dict()
+		# self.complete_link_sim = dict()
+		# self.mean_link_sim = dict()
+	def absorb(self, cluster):
+		self.points.update(cluster.points)
+
+	def __len__(self):
+		return len(self.points)
 
 def dotDistance(s1, s2):
 	return np.dot(s1.point - s2.point, s1.point - s2.point)
@@ -30,7 +40,7 @@ def meanLink(S1, S2):
 	a2 = (1/len(S2))*sum(s.point for s in S2)
 	return dotDistance(a1, a2)
 
-
+# initial loading
 c1 = open('C1.txt')
 points = set()
 for line in c1:
@@ -38,14 +48,39 @@ for line in c1:
 	p = np.array([float(i) for i in split_line[1:]])
 	points.add(NamedPoint(split_line[0], p))
 
+# algorithm
+
+# initialization:
+singleLink_clusters = set()
+completeLink_clusters = set()
+meanLink_clusters = set()
 for point in points:
-	for p in points:
-		pass
-		# single_link, complete_link, and mean_link calculations
+	singleLink_clusters.add(Cluster(point))
+	completeLink_clusters.add(Cluster(point))
+	meanLink_clusters.add(Cluster(point))
 
-	sims = []
+def h_clustering(clusters, k, dist_method):
+	clusts = set(clusters)
+	while len(clusts) > k:
+		pairwise_clusters = set(product(clusts, clusts))
+		arg_mins = None
+		m = float("inf")
+		for c1, c2 in pairwise_clusters:
+			value = dist_method(c1, c2)
+			if value < m:
+				m = value
+				arg_mins = (c1, c2)
+		if arg_mins is None:
+			print('wtf')
+		c1, c2 = arg_mins
+		if len(c1) < len(c2):
+			c2.absorb(c1)
+			clusts -= c1
+		else:
+			c1.absorb(c2)
+			clusts -= c2
+	return clusts
 
-	# sim_values[point.int_id] =
-
-# Step 2 - create hierarchical methods (3)
-
+sl_clusts = h_clustering(singleLink_clusters, 4, singleLink)
+cl_clusts = h_clustering(completeLink_clusters, 4, completeLink)
+ml_clusts = h_clustering(meanLink_clusters, 4, meanLink)
